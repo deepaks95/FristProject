142272d960e2f7406de64ecd42c43c37d94c5fa2
deepaks95 <deecool95@gmail.com>
2022-09-03 23:24:23 +0530

    Dictionary

--- simpledictionary.py
+++ simpledictionary.py
@@ -1,4 +1,8 @@
-mydict = {"Fname":"Deepak","Lname":"suryawanshi"}
+mydict = {"Fname":"Deepak","Lname":"suryawanshi","Address":"Latur","Mob":12345,}
 print(mydict.values())
 print(mydict.keys())
+print(mydict.pop("Lname"))
+print(mydict)
+print(mydict.popitem())
+#print(mydict.clear())
 # git this is comment
