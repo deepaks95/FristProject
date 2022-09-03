1b879e552351d7f02007c7ab4c6743d2e3afabe4
deepaks95 <deecool95@gmail.com>
2022-09-03 23:41:43 +0530

    filter added

--- lambdaProgram.py
+++ lambdaProgram.py
@@ -5,3 +5,6 @@ print(list(map_object))
 
 mydata = [20,30,50,30,30,50,60]
 print(reduce(lambda x,y:x*y,mydata))
+myfilterlist = [75,87,90,99,50,60]
+result=list(filter(lambda x:(x % 15 == 0),myfilterlist))
+print(result)
